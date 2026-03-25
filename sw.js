ac123e41f10f625991cbeccb870680e45de2bb82
SKT Kontrol <sktkontrol@example.com>
2026-03-25 21:07:23 +0300

    Uygulama PWA'ya dönüştürüldü ve ana ekrana eklenebilir hale getirildi

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'skt-kontrol-v36-premium';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://unpkg.com/html5-qrcode',
+    'https://unpkg.com/dexie/dist/dexie.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(caches.keys().then(cacheNames => Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+    )));
+});
